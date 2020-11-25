8e6a7484803b3319292ed5b11d37d9c54003b9c1
github username <samson.yuwono@gmail.com>
2020-11-25 12:34:25 -0500

    first commit @ module 6

---
+++ starter-files/gatsby/src/pages/pizzas.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Nav from '../components/Nav';
+
+export default function PizzasPage() {
+  return (
+    <Layout>
+      <p>Hey! I'm the Pizzas page</p>
+    </Layout>
+  );
+}
