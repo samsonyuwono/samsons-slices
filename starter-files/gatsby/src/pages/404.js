ed4ea905ac2d79ab6a515df5f9fc5ef84305601d
github username <samson.yuwono@gmail.com>
2020-12-29 17:46:45 -0500

    Remove stepped-solutions

--- starter-files/gatsby/src/pages/404.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function FourOhFourPage() {
-  return (
-    <div>
-      <p>Hey - That page doesn't exist</p>
-    </div>
-  );
-}
--- starter-files/gatsby/src/templates/Topping.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function SingleToppingPage({ data: { pizza } }) {
-  // render pizza here
-  console.log(pizza);
-  return <h1>This is a topping</h1>;
-}
